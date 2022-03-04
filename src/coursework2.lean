68217d39ef85cba051024dbea05b81ad989db514
Joey Teng <joey.teng.dev@gmail.com>
2022-03-04 12:36:58 +0000

    feat(coursework 2): submitted version

--- src/coursework2.lean
+++ src/coursework2.lean
@@ -14,6 +14,46 @@ universe u
 namespace simple_graph
 variables {V : Type u} (G : simple_graph V)
 
+/-!
+## Suppliments / Refinement to Connectivity
+
+-/
+namespace walk
+variables {G} [decidable_eq V]
+
+lemma support_drop_until_in {v w : V} (p : G.walk v w) (u : V) (h : u ∈ p.support): u ∈ (p.drop_until u h).support :=
+begin
+  finish,
+end
+
+lemma support_bypass_subset' {u v : V} (p : G.walk u v) : p.support ⊆
+ p.bypass.support :=
+begin
+  induction p,
+  { simp!, },
+  { simp! only,
+    split_ifs,
+    {
+      have h : p_p.bypass.support ⊆ p_p.support,
+      { exact support_bypass_subset p_p, },
+      by_cases hcases : p_u ∈ p_p.support,
+      {
+        intros v' h'',
+        cases h'',
+        { finish, },
+        {
+          sorry,
+        },
+      },
+      { tauto, },
+    },
+    { rw support_cons,
+      apply list.cons_subset_cons,
+      assumption, }, },
+end
+
+end walk
+
 
 /-!
 ## Connectivity
@@ -21,65 +61,14 @@ variables {V : Type u} (G : simple_graph V)
 Utilities about `walk`, `trail` and `path`.
 -/
 
--- def trail_from_walk [decidable_eq V]: Π {v w : V}, G.walk v w → G.walk v w
--- | _ _ walk.nil := walk.nil
--- | x _ (walk.cons p q) :=
---   if q.edges.nodup then walk.cons p q else walk.cons p q
-
--- def walk_drop_loop [decidable_eq V]: Π {u v w : V}, G.adj u v → G.walk u w → G.walk v w
--- | _ v w _ (walk.cons _ walk.nil) := G.walk v w
--- | u v w adj (walk.cons a t) := if
-
 def subwalk_nil_from_vertex : Π {u : V}, G.walk u u
 | u := (walk.nil : G.walk u u)
-
--- def walk_from_edges : Π {u v : V}, list (sym2 V) → G.walk u v
--- | u v (a :: []) := walk.cons (a : G.adj u v) (walk.nil : G.walk v v)
-
--- def subwalk_from_adj [decidable_eq V]:
---   Π {u v w x: V}, G.adj u v → G.walk w x → G.walk u x
--- | u _ _ x adj walk.nil := walk.cons adj (walk.nil : G.walk x x)
--- | u v w x _adj (walk.cons (adj _ _) q) :=
---   if p u v then walk.cons p q else subwalk_from_vertex u q
-
 
 theorem is_trail_nil {v : V} (p : G.walk v v): p = walk.nil → p.is_trail :=
 begin
   intro hp,
   rw hp,
   simp,
-end
-
-
-theorem has_trail_iff_walk {v w : V} (p : G.walk v w) [decidable_eq V]:
-  ∃ ⦃p' : G.walk v w⦄, p'.is_trail :=
-begin
-  induction p with d v_ mid w_ adj walk_ hd,
-  { dsimp at *,
-    fsplit,
-    work_on_goal 0 { refl },
-    fsplit,
-    exact dec_trivial,
-  },
-  { cases hd with trail_ h,
-    -- by_cases hdup : adj ∈ trail_.edges, -- not working
-    -- Should discuss by cases
-    -- If adj is a dup, then roll-back all the way s.t. adj is not a dup
-    -- Else append_left
-    have t' := walk.cons adj trail_,
-    have htcons : t' = walk.cons adj trail_,
-    {
-      sorry,
-    },
-    have ht' : t'.is_trail,
-    {
-      sorry,
-      -- rw t',
-      -- rw walk.cons_is_trail_iff,
-    },
-    use t',
-    exact ht'
-  },
 end
 
 /-!
@@ -122,12 +111,13 @@ begin
   use p,
 end
 
--- @[simp]
+@[simp]
 theorem reachable_symm {u v : V} :
-  G.reachable u v → G.reachable v u :=
+  G.reachable u v ↔ G.reachable v u :=
 begin
-  intro h,
-  cases h with p _,
+  split;
+  intro h;
+  cases h with p _;
   use p.reverse,
 end
 
@@ -142,36 +132,78 @@ begin
   use p,
 end
 
-theorem reachable_if_passing {u v w : V} (p : G.walk u w):
-  v ∈ p.support → G.reachable v w :=
+theorem reachable_if_passing {u v w : V} (p : G.walk u w) (hp : v ∈ p.support) [decidable_eq V]:
+  G.reachable v w :=
 begin
-  intro hp,
+  rw reachable_def,
   fconstructor,
   {
-    haveI support := p.support,
-    sorry,
+    let p' : G.walk u w := p.to_path,
+    have hp' : v ∈ p'.support,
+    {
+      apply walk.support_bypass_subset',
+      exact hp,
+    },
+    have hp'' : p'.is_path,
+    { exact walk.bypass_is_path p, },
+    let q : G.walk v w := p'.drop_until v hp',
+    use q,
   },
   { triv },
 end
 
-theorem reachable_if_support {u v w x : V} :
-  ∃ {p : G.walk u x}, v ∈ p.support ∧ w ∈ p.support → G.reachable v w :=
+theorem reachable_if_support {u v w x : V} (p : G.walk u x) (h1 : v ∈ p.support) (h2 : w ∈ p.support) [decidable_eq V]:
+  G.reachable v w :=
 begin
-  sorry,
+  have h' : G.reachable v x,
+  { apply reachable_if_passing G p h1, },
+  have h'' : G.reachable w x,
+  { apply reachable_if_passing G p h2, },
+  have h''' : G.reachable x w,
+  {
+    rw G.reachable_symm at h'',
+    exact h'',
+  },
+
+  -- Unsure how to use "reachable_trans", thus copy over
+  cases h' with p1 hp1,
+  cases h''' with p2 hp2,
+  have p := p1.append p2,
+  use p,
 end
 
-/-### Extend to trail-/
+/-### Extend to path-/
 
-theorem reachable_trail {v w : V} :
-  G.reachable v w ↔ ∃ ⦃p : G.walk v w⦄, p.is_trail :=
+theorem reachable_path {v w : V} [decidable_eq V]:
+  G.reachable v w ↔ ∃ ⦃p : G.walk v w⦄, p.is_path :=
 begin
   split,
   { simp,
     intro h,
     rw reachable at h,
     cases h with p _,
+    let p' : G.walk v w := p.bypass,
+    use p',
+    exact walk.bypass_is_path p,
+  },
+  { simp,
+    intros x hx,
+    use x,
+  },
+end
+
+/-### Extend to trail-/
+
+theorem reachable_trail {v w : V} [decidable_eq V]:
+  G.reachable v w ↔ ∃ ⦃p : G.walk v w⦄, p.is_trail :=
+begin
+  split,
+  { rw reachable_path,
+    intro h,
+    cases h with p hp,
     use p,
-    sorry,
+    let p' := hp.to_trail,
+    exact p',
   },
   { simp,
     intros x hx,
@@ -210,7 +242,7 @@ section Eulerian
 variables [decidable_eq V]
 
 def is_euler_circuit {v : V} (p : G.walk v v) : Prop :=
-  p.is_circuit ∧ ∀ ⦃e : sym2 V⦄, e ∈ G.edge_set → (e ∈ p.edges)
+  p.is_circuit ∧ (∀ ⦃e : sym2 V⦄, e ∈ G.edge_set → (e ∈ p.edges)) ∧ ∀ ⦃u : V⦄, u ∈ p.support
 
 def is_eulerian [decidable_eq V] : Prop :=
   ∃ {v : V}, ∃ {p : G.walk v v}, G.is_euler_circuit p
@@ -220,15 +252,43 @@ theorem eulerian_is_connected :
 begin
   rw is_eulerian,
   intro h,
-  cases h with v hv,
-  cases hv with p hp,
-  sorry,
+  cases h with u hu,
+  cases hu with p hp,
+  rw is_euler_circuit at hp,
+  obtain ⟨hp, he, hV⟩ := hp,
+  rw is_connected,
+  intros v w,
+  have hv : v ∈ p.support,
+  { tauto, },
+  have hw : w ∈ p.support,
+  { tauto, },
+  exact G.reachable_if_support p hv hw,
 end
 
 theorem eulerian_all_even_degree [decidable_rel G.adj] [fintype V] :
   G.is_eulerian → ∀ (v : V), even (G.degree v):=
 begin
-  sorry
+  intro h,
+  cases h with u hu,
+  cases hu with p hp,
+  obtain ⟨hp, he, hV⟩ := hp,
+
+  intro v,
+  have hv : v ∈ p.support,
+  { apply hV, },
+  let pe := p.edges,
+  let deg := G.degree v,
+  have hdeg : even deg,
+  {
+    -- Either
+    -- - v == u, thus list's first sym2 has a v, and last sym2 has a v
+    -- - v != u, thus
+      -- for each (sym2 V), i-th element is (_, v) → i+1-th element is (v, __)
+      -- hence (sym2 V) comes in pairs
+      -- thus degree is even.
+    sorry,
+  },
+  exact hdeg,
 end
 
 /-Euler's Theorem-/
@@ -291,62 +351,109 @@ begin
   exact dec_trivial
 end
 
--- example {G : simple_graph sector_}:
---   graph_2.is_eulerian :=
--- calc
---    graph_2.is_eulerian
---   = ∃ {v : sector_}, ∃ {p : graph_2.walk v v}, graph_2.is_euler_circuit p : by rw is_eulerian
---   -- = : sorry,
-
-example {G : simple_graph sector_}:
+example {G : simple_graph sector_} [decidable_rel G.adj] [fintype sector_]:
    G = graph_2 → G.is_eulerian :=
 begin
   intro h,
-  have v1 := sector_.v1,
-  have v2 := sector_.v2,
-  have v3 := sector_.v3,
-  have v4 := sector_.v4,
+  -- construct such a circuit
+  let v1 := sector_.v1,
+  let v2 := sector_.v2,
+  let v3 := sector_.v3,
+  let v4 := sector_.v4,
   rw is_eulerian,
   use v4,
-  have p' : G.walk v4 v4 := walk.nil,
+  let p' : G.walk v4 v4 := walk.nil,
   have e14 : G.adj v1 v4,
-  { rw h,
-    split,
-    { sorry, },
-    { have h_ := bridge_2(v1)(v4),
-      sorry,
-    },
+  {
+    rw h,
+    fconstructor;
+    tauto,
   },
   have e21 : G.adj v2 v1,
-  { sorry, }, -- same as e14
+  {
+    rw h,
+    fconstructor;
+    tauto,
+  }, -- same as e14
   have e32 : G.adj v3 v2,
-  { sorry, },
+  {
+    rw h,
+    fconstructor;
+    tauto,
+  },
   have e43 : G.adj v4 v3,
-  { sorry, },
-  have p : G.walk v4 v4 := walk.cons e43 (walk.cons e32 (walk.cons e21 (walk.cons e14 p'))),
+  {
+    rw h,
+    fconstructor;
+    tauto,
+  },
+  let p : G.walk v4 v4 := walk.cons e43 (walk.cons e32 (walk.cons e21 (walk.cons e14 p'))),
+  -- END of circuit construction
   use p,
   rw is_euler_circuit,
   split,
   {
-    split,
+    fconstructor,
     {
-      split,
-      rw list.nodup,
+      fconstructor,
+      exact dec_trivial,
+    },
+    { exact dec_trivial, },
+  },
+  split,
+  -- prove edges
+  {
+    intro e,
+    intro he,
+    let _set := G.edge_set,
+    rw ← mem_edge_set at *,
+    have h_set : _set = G.edge_set,
+    { refl, },
+    have hset : graph_2.edge_set = {⟦(v1, v4)⟧, ⟦(v2, v1)⟧, ⟦(v3, v2)⟧, ⟦(v4, v3)⟧},
+    {
       sorry,
     },
+    rw ← h at hset,
+    rw hset at h_set,
+    norm_num,
+    by_cases hc : e = ⟦(v4, v3)⟧,
+    { left, exact hc, },
     {
-      intro h,
-      sorry, -- by the construction of p
+      by_cases hc' : e = ⟦(v3, v2)⟧,
+      { right, left, exact hc', },
+      {
+        by_cases hc'' : e = ⟦(v2, v1)⟧,
+        { right, right, left, exact hc'', },
+        {
+          by_cases hc''' : e = ⟦(v1, v4)⟧,
+          { right, right, right, exact hc''', },
+          { finish, },
+        },
+      },
     },
   },
-  intro e,
-  intro he,
-  have _set := G.edge_set,
-  induction e,
-  { cases e,
-    sorry,
+  -- proving vertices
+  {
+    intro u,
+    norm_num,
+    cases u,
+    {
+      right, right, right, left,
+      refl,
+    },
+    {
+      right, right, left,
+      refl,
+    },
+    {
+      right, left,
+      refl,
+    },
+    {
+      left,
+      refl,
+    },
   },
-  { refl },
 end
 
 end Konigsberg
