315e5e5f2d2dcb43160e323fb8c596dcd540b5bf
Joey Teng <joey.teng.dev@gmail.com>
2022-04-01 04:06:05 +0100

    feat(coursework 3): a fully working example of proving .is_eulerian

--- src/coursework3.lean
+++ src/coursework3.lean
@@ -210,12 +210,17 @@ end
 section Konigsberg
 
 /- Four sectors -/
-@[derive decidable_eq]
+@[derive [decidable_eq, fintype]] -- both necessary
 inductive sector_
 | v1 : sector_
 | v2 : sector_
 | v3 : sector_
 | v4 : sector_
+
+/-
+instance : fin_enum sector_ := -- not necessary
+  fin_enum.of_list [sector_.v1, sector_.v2, sector_.v3, sector_.v4] (λ x, by cases x; simp)
+-/
 
 /-
   v1
@@ -224,10 +229,10 @@ inductive sector_
   | /
   v3
 -/
-def bridge_ : sector_ → sector_ → Prop
-| sector_.v1 sector_.v3 := false
-| sector_.v3 sector_.v1 := false
-| _ _ := true
+@[derive decidable_rel]
+def bridge_ (a b : sector_) : Prop :=
+  a ≠ b ∧
+  ¬ ((a = sector_.v1 ∧ b = sector_.v3) ∨ (a = sector_.v3 ∧ b = sector_.v1))
 
 /-
   A Euler Graph
@@ -238,62 +243,68 @@ def bridge_ : sector_ → sector_ → Prop
    \ /
     v3
 -/
-def bridge_2 : sector_ → sector_ → Prop
-| sector_.v1 sector_.v3 := false
-| sector_.v3 sector_.v1 := false
-| sector_.v2 sector_.v4 := false
-| sector_.v4 sector_.v2 := false
-| _ _ := true
+@[derive decidable_rel]
+def bridge_2 (a b : sector_) : Prop :=
+/- (a = sector_.v1 ∧ b = sector_.v2) ∨ -/ (a = sector_.v2 ∧ b = sector_.v1) ∨
+/- (a = sector_.v2 ∧ b = sector_.v3) ∨ -/ (a = sector_.v3 ∧ b = sector_.v2) ∨
+/- (a = sector_.v3 ∧ b = sector_.v4) ∨ -/ (a = sector_.v4 ∧ b = sector_.v3) ∨
+/- (a = sector_.v4 ∧ b = sector_.v1) ∨ -/ (a = sector_.v1 ∧ b = sector_.v4)
+
 
 def graph_ : simple_graph sector_ := simple_graph.from_rel bridge_
+-- @[derive decidable] -- not necessary
 def graph_2 : simple_graph sector_ := simple_graph.from_rel bridge_2
 
-example : sector_.v1 ≠ sector_.v4 :=
+
+theorem mem_edge_set_adj {pair : prod V V} : ⟦pair⟧ ∈ G.edge_set ↔ G.adj pair.fst pair.snd :=
 begin
-  exact dec_trivial
+  split,
+  { exact (mem_edge_set G).mp, },
+  { exact (mem_edge_set G).mpr, },
 end
 
-example {G : simple_graph sector_} [decidable_rel G.adj] [fintype sector_]:
-   G = graph_2 → G.is_eulerian :=
+example : graph_2.is_eulerian :=
 begin
-  intro h,
   -- construct such a circuit
   let v1 := sector_.v1,
   let v2 := sector_.v2,
   let v3 := sector_.v3,
   let v4 := sector_.v4,
+  have v1_eq : v1 = sector_.v1 := by refl,
+  have v2_eq : v2 = sector_.v2 := by refl,
+  have v3_eq : v3 = sector_.v3 := by refl,
+  have v4_eq : v4 = sector_.v4 := by refl,
   rw is_eulerian,
   use v4,
-  let p' : G.walk v4 v4 := walk.nil,
-  have e14 : G.adj v1 v4,
+  let p' : graph_2.walk v4 v4 := walk.nil,
+  have e14 : graph_2.adj v1 v4,
   {
-    rw h,
     fconstructor;
-    tauto,
+    { exact dec_trivial, },
   },
-  have e21 : G.adj v2 v1,
+  have e21 : graph_2.adj v2 v1,
   {
-    rw h,
     fconstructor;
-    tauto,
+    { exact dec_trivial, },
+
   }, -- same as e14
-  have e32 : G.adj v3 v2,
+  have e32 : graph_2.adj v3 v2,
   {
-    rw h,
     fconstructor;
-    tauto,
+    { exact dec_trivial, },
+
   },
-  have e43 : G.adj v4 v3,
+  have e43 : graph_2.adj v4 v3,
   {
-    rw h,
     fconstructor;
-    tauto,
+    { exact dec_trivial, },
   },
-  let p : G.walk v4 v4 := walk.cons e43 (walk.cons e32 (walk.cons e21 (walk.cons e14 p'))),
+  let p : graph_2.walk v4 v4 := walk.cons e43 (walk.cons e32 (walk.cons e21 (walk.cons e14 p'))),
   -- END of circuit construction
   use p,
   rw is_euler_circuit,
   split,
+  -- prove p.is_circuit -- trivial
   {
     fconstructor,
     {
@@ -305,33 +316,198 @@ begin
   split,
   -- prove edges
   {
-    intro e,
-    intro he,
-    let _set := G.edge_set,
-    rw ← mem_edge_set at *,
-    have h_set : _set = G.edge_set,
-    { refl, },
-    have hset : graph_2.edge_set = {⟦(v1, v4)⟧, ⟦(v2, v1)⟧, ⟦(v3, v2)⟧, ⟦(v4, v3)⟧},
+    intros e,
+    apply quotient.induction_on e,
+    clear e,
+
+    intro a',
+    rw mem_edge_set_adj,
+    revert a',
+    rw prod.forall,
+    intros a b,
+    have tmp_rw_a : (a, b).fst = a, by exact rfl,
+    have tmp_rw_b : (a, b).snd = b, by exact rfl,
+    rw tmp_rw_a,
+    rw tmp_rw_b,
+    clear tmp_rw_a tmp_rw_b,
+    unfold walk.edges,
+    intro hadj,
+    rw list.mem_map,
+    let d : graph_2.dart := {to_prod := (a, b), is_adj := hadj},
+    by_cases hd : d ∈ p.darts,
     {
-      sorry,
-      -- try to prove by A ⊆ B ∧ B ⊆ A → A = B
+      use d,
+      split,
+      { exact hd, },
+      { refl, },
     },
-    rw ← h at hset,
-    rw hset at h_set,
-    norm_num,
-    by_cases hc : e = ⟦(v4, v3)⟧,
-    { left, exact hc, },
     {
-      by_cases hc' : e = ⟦(v3, v2)⟧,
-      { right, left, exact hc', },
+      let d' : graph_2.dart := {to_prod := (b, a), is_adj := hadj.symm},
+      have hd' : d' ∈ p.darts,
       {
-        by_cases hc'' : e = ⟦(v2, v1)⟧,
-        { right, right, left, exact hc'', },
+        norm_num,
+        norm_num at hd,
+        repeat { rw not_or_distrib at hd, },
+        repeat { rw dart.ext_iff at hd ⊢, },
+        -- dsimp at hd h_contra,
+        simp at hd ⊢,
+
+        unfold graph_2 at hadj,
+        rw from_rel_adj at hadj,
+        obtain ⟨neq, hadj'⟩ := hadj,
+        obtain ⟨n43, n32, n21, n14⟩ := hd,
+        -- obtain ⟨n34, n23, n12, n41⟩ := h_contra,
+        by_cases ha : a = v1,
         {
-          by_cases hc''' : e = ⟦(v1, v4)⟧,
-          { right, right, right, exact hc''', },
-          { finish, },
+          right, right, left,
+          by_contra h_contra,
+          rw [not_and_distrib, ha] at h_contra,
+          norm_num at h_contra,
+          have hbn1 : ¬b = v1,
+          {
+            rw ha at neq,
+            dsimp at neq,
+            rw eq_comm at neq,
+            exact neq,
+          },
+          have hbn4 : ¬b = v4, by exact n14 ha,
+          have hbn2 := h_contra,
+          have hbn3 : ¬b = v3,
+          {
+            by_contra bv3,
+            rw [ha, bv3, v1_eq, v3_eq] at hadj',
+            unfold bridge_2 at hadj',
+            norm_num at hadj',
+          },
+          -- eliminate as has checked exhaustively; similar / same block has been used 5 times.
+          clear_except v1_eq v2_eq v3_eq v4_eq hbn1 hbn2 hbn3 hbn4,
+          have hb : ¬ b = v1 ∧ ¬ b = v2 ∧ ¬ b = v3 ∧ ¬ b = v4, by tauto,
+          rw [v1_eq, v2_eq, v3_eq, v4_eq] at hb,
+          clear_except hb,
+          repeat {rw ← not_or_distrib at hb, },
+          let verts : finset sector_ := sector_.fintype.elems,
+          have hb' : b ∈ verts, by apply sector_.fintype.complete,
+          repeat { cases hb', };
+          norm_num at hb,
         },
+        by_cases ha2 : a = v2,
+        {
+          right, left,
+          by_contra h_contra,
+          rw [not_and_distrib, ha2] at h_contra,
+          norm_num at h_contra,
+          have hbn2 : ¬b = v2,
+          {
+            rw ha2 at neq,
+            dsimp at neq,
+            rw eq_comm at neq,
+            exact neq,
+          },
+          have hbn1 : ¬b = v1, by exact n21 ha2,
+          have hbn3 := h_contra,
+          have hbn4 : ¬b = v4,
+          {
+            by_contra bv4,
+            rw [ha2, bv4, v2_eq, v4_eq] at hadj',
+            unfold bridge_2 at hadj',
+            norm_num at hadj',
+          },
+          -- eliminate as has checked exhaustively; same as above
+          clear_except v1_eq v2_eq v3_eq v4_eq hbn1 hbn2 hbn3 hbn4,
+          have hb : ¬ b = v1 ∧ ¬ b = v2 ∧ ¬ b = v3 ∧ ¬ b = v4, by tauto,
+          rw [v1_eq, v2_eq, v3_eq, v4_eq] at hb,
+          clear_except hb,
+          repeat {rw ← not_or_distrib at hb, },
+          let verts : finset sector_ := sector_.fintype.elems,
+          have hb' : b ∈ verts, by apply sector_.fintype.complete,
+          repeat { cases hb', };
+          norm_num at hb,
+        },
+        by_cases ha3 : a = v3,
+        {
+          left,
+          by_contra h_contra,
+          rw [not_and_distrib, ha3] at h_contra,
+          norm_num at h_contra,
+          have hbn3 : ¬b = v3,
+          {
+            rw ha3 at neq,
+            dsimp at neq,
+            rw eq_comm at neq,
+            exact neq,
+          },
+          have hbn2 : ¬b = v2, by exact n32 ha3,
+          have hbn4 := h_contra,
+          have hbn1 : ¬b = v1,
+          {
+            by_contra bv1,
+            rw [ha3, bv1, v3_eq, v1_eq] at hadj',
+            unfold bridge_2 at hadj',
+            norm_num at hadj',
+          },
+          -- eliminate as has checked exhaustively; same as above
+          clear_except v1_eq v2_eq v3_eq v4_eq hbn1 hbn2 hbn3 hbn4,
+          have hb : ¬ b = v1 ∧ ¬ b = v2 ∧ ¬ b = v3 ∧ ¬ b = v4, by tauto,
+          rw [v1_eq, v2_eq, v3_eq, v4_eq] at hb,
+          clear_except hb,
+          repeat {rw ← not_or_distrib at hb, },
+          let verts : finset sector_ := sector_.fintype.elems,
+          have hb' : b ∈ verts, by apply sector_.fintype.complete,
+          repeat { cases hb', };
+          norm_num at hb,
+        },
+        by_cases ha4 : a = v4,
+        {
+          repeat { right, },
+          by_contra h_contra,
+          rw [not_and_distrib, ha4] at h_contra,
+          norm_num at h_contra,
+          have hbn4 : ¬b = v4,
+          {
+            rw ha4 at neq,
+            dsimp at neq,
+            rw eq_comm at neq,
+            exact neq,
+          },
+          have hbn3 : ¬b = v3, by exact n43 ha4,
+          have hbn1 := h_contra,
+          have hbn2 : ¬b = v2,
+          {
+            by_contra bv2,
+            rw [ha4, bv2, v4_eq, v2_eq] at hadj',
+            unfold bridge_2 at hadj',
+            norm_num at hadj',
+          },
+          -- eliminate as has checked exhaustively; same as above
+          clear_except v1_eq v2_eq v3_eq v4_eq hbn1 hbn2 hbn3 hbn4,
+          have hb : ¬ b = v1 ∧ ¬ b = v2 ∧ ¬ b = v3 ∧ ¬ b = v4, by tauto,
+          rw [v1_eq, v2_eq, v3_eq, v4_eq] at hb,
+          clear_except hb,
+          repeat {rw ← not_or_distrib at hb, },
+          let verts : finset sector_ := sector_.fintype.elems,
+          have hb' : b ∈ verts, by apply sector_.fintype.complete,
+          repeat { cases hb', };
+          norm_num at hb,
+        },
+        {
+          -- eliminate as has checked exhaustively; similar to above
+          clear_except v1_eq v2_eq v3_eq v4_eq ha ha2 ha3 ha4,
+          have ha : ¬ a = v1 ∧ ¬ a = v2 ∧ ¬ a = v3 ∧ ¬ a = v4, by tauto,
+          rw [v1_eq, v2_eq, v3_eq, v4_eq] at ha,
+          clear_except ha,
+          repeat {rw ← not_or_distrib at ha, },
+          let verts : finset sector_ := sector_.fintype.elems,
+          have ha' : a ∈ verts, by apply sector_.fintype.complete,
+          repeat { cases ha', };
+          norm_num at ha,
+        },
+      },
+      use d',
+      split,
+      { exact hd', },
+      {
+        unfold dart.edge,
+        rw sym2.eq_swap,
       },
     },
   },
